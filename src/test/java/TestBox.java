019a66a3d6bc60787df526a7bb5f83a9f0f15268
Tombervilt <bad_hippie@mail.ru>
2025-04-21 19:40:18 +0300

    complete form "automation-practice-form" filing

--- src/test/java/TestBox.java
+++ src/test/java/TestBox.java
@@ -1,6 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -21,19 +24,21 @@ public class TestBox {
             $("#firstName").setValue("Thomas");
             $("#lastName").setValue("Anderson");
             $("#userEmail").setValue("neo@matrix.web");
-            $("#gender-radio-1").selectRadio("Male");
+            $("label[for='gender-radio-1']").click();
             $("#userNumber").setValue("8800555353");
-            /*$("#hobbies-checkbox-1").click();
-            $("").setValue("");
-            $("").setValue("");
-            $("").setValue("");
-            $("").setValue("");
-            $("").setValue("");
-            $("").setValue("");
-            $("").setValue("");
-            $("").setValue("");
-            $("").setValue("");
-            $("#submit").click();*/
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("September");
+            $(".react-datepicker__year-select").selectOption("1971");
+            $("div[aria-label='Choose Monday, September 13th, 1971']").click();
+            $("#subjectsInput").setValue("English").pressEnter();
+           // $("#subjectsInput").setValue("A"); это мне на потом потренироваться кое-что. Не стер, чтобы не забыть
+            $("label[for=hobbies-checkbox-1]").click();
+            $("label[for=hobbies-checkbox-3]").click();
+            $("#uploadPicture").uploadFile(new File("src/test/resourses/image/test.png"));
+            $("#currentAddress").setValue("г. Н, ул. Пушкина, д. Колотушкина, кв. 101");
+            $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+            $("#react-select-4-input").setValue("Agra").pressEnter();
+            $("#submit").click();
            sleep(5000);
 
         }
