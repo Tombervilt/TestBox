f0f224cbda424fb612d6818ae4ba61e629197616
Tombervilt <bad_hippie@mail.ru>
2025-04-23 22:19:16 +0300

    added final form validation test

--- src/test/java/TestBox.java
+++ src/test/java/TestBox.java
@@ -31,15 +31,24 @@ public class TestBox {
             $(".react-datepicker__year-select").selectOption("1971");
             $("div[aria-label='Choose Monday, September 13th, 1971']").click();
             $("#subjectsInput").setValue("English").pressEnter();
-           // $("#subjectsInput").setValue("A"); это мне на потом потренироваться кое-что. Не стер, чтобы не забыть
             $("label[for=hobbies-checkbox-1]").click();
             $("label[for=hobbies-checkbox-3]").click();
             $("#uploadPicture").uploadFile(new File("src/test/resourses/image/test.png"));
             $("#currentAddress").setValue("г. Н, ул. Пушкина, д. Колотушкина, кв. 101");
             $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
-            $("#react-select-4-input").setValue("Agra").pressEnter();
-            $("#submit").click();
-           sleep(5000);
+            $("#react-select-4-input").setValue("Agra").pressEnter();$("#submit").click();
+
+            $("div[role=document]").shouldHave(text("Thomas Anderson"));
+            $("div[role=document]").shouldHave(text("neo@matrix.web"));
+            $("div[role=document]").shouldHave(text("Male"));
+            $("div[role=document]").shouldHave(text("8800555353"));
+            $("div[role=document]").shouldHave(text("13 September,1971"));
+            $("div[role=document]").shouldHave(text("English"));
+            $("div[role=document]").shouldHave(text("Sports, Music"));
+            $("div[role=document]").shouldHave(text("test.png"));
+            $("div[role=document]").shouldHave(text("г. Н, ул. Пушкина, д. Колотушкина, кв. 101"));
+            $("div[role=document]").shouldHave(text("Uttar Pradesh Agra"));
+
 
         }
 
